aeb4abba8bc049d9ea6c24beb765f45e8a7ed876
jackripttt <120683404+jackripttt@users.noreply.github.com>
2023-10-22 19:57:55 -0600

    Update file_server.py

--- file_server.py
+++ file_server.py
@@ -1,18 +1,45 @@
 import http.server
 import socketserver
+import os
+from urllib.parse import parse_qs
 
 # Define the port on which the server will listen.
 PORT = 8000
 
-# Define the request handler. This will process incoming requests and
-# provide responses. In this case, we're using the simple HTTP request
-# handler provided by Python's http.server module.
-Handler = http.server.SimpleHTTPRequestHandler
+class MyHTTPRequestHandler(http.server.SimpleHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'text/html')
+        self.end_headers()
+        message = '''
+        <html>
+        <body>
+            <form method="POST">
+                <input name="input_text" type="text" />
+                <input type="submit" />
+            </form>
+        </body>
+        </html>
+        '''
+        self.wfile.write(bytes(message, "utf8"))
+        return
+
+    def do_POST(self):
+        content_length = int(self.headers['Content-Length'])
+        post_data = self.rfile.read(content_length)
+        data = parse_qs(post_data)
+
+        self.send_response(200)
+        self.send_header('Content-type', 'text/html')
+        self.end_headers()
+        response = "You submitted: {}".format(data.get('input_text')[0])
+        self.wfile.write(bytes(response, "utf8"))
+        return
 
 # Create a TCP server instance. This will start a server that listens for
 # incoming TCP connections on the specified port. When a connection is
 # established, it will use the specified handler to process requests.
-with socketserver.TCPServer(("", PORT), Handler) as httpd:
+with socketserver.TCPServer(("", PORT), MyHTTPRequestHandler) as httpd:
     print(f"Serving at port {PORT}")
     # This command actually starts the server. It will run forever, waiting
     # for connections and serving files.
